0d10ccbc52da7bb5eeb038a818af5d2cdd49467d
Maurice Koster <maurice.koster@ing.nl>
2014-10-21 16:20:11 +0200

    added some functions

--- Modules/DbOracle/DbOracle.psm1
+++ Modules/DbOracle/DbOracle.psm1
@@ -4,6 +4,13 @@ $odpAssemblyName = "Oracle.DataAccess, Version=2.112.3.0, Culture=neutral, Publi
 # $asm = [appdomain]::currentdomain.getassemblies() | where-object {$_.FullName -eq $odpAssemblyName}
 # $asm.GetTypes() | Where-Object {$_.IsPublic} | Sort-Object {$_.FullName } | ft FullName, BaseType | Out-String
 
+function Select-TNS
+{
+    $enu = New-Object Oracle.DataAccess.Client.OracleDataSourceEnumerator
+    Write-Output $enu.GetDataSources()
+}
+
+
 function Connect-Oracle([string] $connectionString = $(throw "connectionString is required"))
 {
   $conn= New-Object Oracle.DataAccess.Client.OracleConnection($connectionString)
@@ -15,6 +22,19 @@ function Get-ConnectionString($user, $pass, $hostName, $port, $sid)
 {
   $dataSource = ("(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={0})  (PORT={1}))(CONNECT_DATA=(SERVICE_NAME={2})))" -f $hostName, $port,  $sid)
   Write-Output ("Data Source={0};User Id={1};Password={2};Connection Timeout=10" -f $dataSource, $user, $pass)
+}
+
+function Get-ConfigConnectionString(
+    [string] $filename = $(throw "filename is required"),
+    [string] $name = $(throw "connection string name is required")
+    )
+{
+    $config = [xml](Get-Content $filename)
+    $item = $config.configuration.connectionStrings.add | where {$_.name -eq $name}
+    if (!$item) { 
+        throw "Failed to find a connection string with name '{0}'" -f $name
+    }
+    return $item.connectionString
 }
 
 function Get-DataTable
